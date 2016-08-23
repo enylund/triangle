f141cd521aee2a82eea662586acc46f9b7d0240b
enylund <nylund.eric@gmail.com>
2016-08-22 19:55:50 -0700

    Add artwork images

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php the_post() ?>
 <div class="content">
   <div id="post">
+  <h2>People</h2>
     <?php
     //the query
     $args = array(
@@ -13,7 +14,8 @@
 
     if($artists->have_posts()) {
       while($artists->have_posts()) {
-        $artists->the_post(); ?>
+        $artists->the_post();
+        $images = get_field('artwork_images'); ?>
 
         <h3 class='artist__name'>
           <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
@@ -21,6 +23,18 @@
         <p class='artist__bio'>
           <?php the_field('bio'); ?>
         </p>
+
+        <?php if( $images ): ?>
+          <ul>
+            <?php foreach( $images as $image ): ?>
+              <li>
+                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+              </li>
+            <?php endforeach; ?>
+          </ul>
+        <?php endif; ?>
+
+
 
       <?php }
       wp_reset_postdata();
