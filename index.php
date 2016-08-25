6c007b3aa777884fdb0651052f8a4fa43f14754f
enylund <nylund.eric@gmail.com>
2016-08-24 18:58:12 -0700

    Add news items to the homepage

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php the_post() ?>
+
+<section class="news">
+  <?php
+
+    $news_array = array(
+      'category_name' => 'news'
+    );
+
+    $news_items = new WP_Query($news_array);
+    if($news_items->have_posts()) {
+      while($news_items->have_posts()) {
+        $news_items->the_post(); ?>
+
+        <div class="news_item_wrapper">
+          <a href="<?php the_field('link'); ?>">
+            <?php the_field('content'); ?>
+          </a>
+        </div>
+
+     <?php }
+      wp_reset_postdata();
+    }?>
+</section>
 <div class="content">
   <div id="post">
   <h2>People</h2>
