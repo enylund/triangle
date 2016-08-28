9b86f9cc02a0638bb119f940e2714d7481524a4a
enylund <nylund.eric@gmail.com>
2016-08-27 20:15:40 -0700

    News section grid

--- partials/news_section.php
+++ partials/news_section.php
@@ -1,34 +1,36 @@
-<section class="news row">
-  <?php
+<section class="news">
+  <div class="row">
+    <?php
 
-    $news_array = array(
-      'category_name' => 'news',
-      'meta_key'  => 'user_date',
-      'orderby' => 'user_date',
-      'order'   => 'DESC'
-    );
+      $news_array = array(
+        'category_name' => 'news',
+        'meta_key'  => 'user_date',
+        'orderby' => 'user_date',
+        'order'   => 'DESC'
+      );
 
-    $news_items = new WP_Query($news_array);
-    if($news_items->have_posts()) {
-      $counter = 0;
-      while($news_items->have_posts() && $counter < 3) {
-        $news_items->the_post();
-        ?>
+      $news_items = new WP_Query($news_array);
+      if($news_items->have_posts()) {
+        $counter = 0;
+        while($news_items->have_posts() && $counter < 3) {
+          $news_items->the_post();
+          ?>
 
-        <div class="col-sm-4 news_item_wrapper">
-          <?php if( get_field('link') ): ?>
-            <a href="<?php the_field('link'); ?>">
+          <div class="col-sm-4 news_item_wrapper">
+            <?php if( get_field('link') ): ?>
+              <a href="<?php the_field('link'); ?>">
+                <?php the_field('content'); ?>
+              </a>
+            <?php else: ?>
+            <div class=''>
               <?php the_field('content'); ?>
-            </a>
-          <?php else: ?>
-          <div class=''>
-            <?php the_field('content'); ?>
+            </div>
+            <?php endif; ?>
           </div>
-          <?php endif; ?>
-        </div>
 
-        <?php $counter++;
-      }
-      wp_reset_postdata();
-    }?>
+          <?php $counter++;
+        }
+        wp_reset_postdata();
+      }?>
+    </div>
 </section>
