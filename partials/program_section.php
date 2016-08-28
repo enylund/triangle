37818622c4d955aa964bba3d757020fcb106f975
enylund <nylund.eric@gmail.com>
2016-08-27 20:48:29 -0700

    Create a program styles sass file

--- partials/program_section.php
+++ partials/program_section.php
@@ -1,35 +1,46 @@
-<h2>Programs</h2>
+<section class="program">
 
-<?php
-    //the query
-    $args = array(
-      'category_name' => 'event',
-      'meta_key'  => 'user_date',
-      'orderby' => 'user_date',
-      'order'   => 'DESC'
-    );
+  <div class="row">
+    <h2 class="col-sm-offset-1 col-sm-10 site-wide__headline">
+      Programs
+    </h2>
 
-    $events = new WP_Query($args);
+    <?php
+        //the query
+        $args = array(
+          'category_name' => 'event',
+          'meta_key'  => 'user_date',
+          'orderby' => 'user_date',
+          'order'   => 'DESC'
+        );
 
-    if($events->have_posts()) {
-      while($events->have_posts()) {
-        $events->the_post();
-        $field = get_field_object('event_type');
-        $value = get_field('event_type');
-        $event_category = $field['choices'][ $value ];
-        $event_title = get_field('title');
-        $event_date = get_field('user_date'); ?>
-        <div class="program">
-          <div class='program_category'>
-            <?php echo $event_category; ?>
-          </div>
-          <div class="">
-            <?php echo $event_title; ?>
-          </div>
-          <div class="">
-          <?php echo $event_date; ?>
-          </div>
-        </div>
-      <?php }
-      wp_reset_postdata();
-}?>
+        $events = new WP_Query($args);
+
+        if($events->have_posts()) {
+          while($events->have_posts()) {
+            $events->the_post();
+            $field = get_field_object('event_type');
+            $value = get_field('event_type');
+            $event_category = $field['choices'][ $value ];
+            $event_title = get_field('title');
+            $event_date = get_field('user_date'); ?>
+
+
+            <div class="program_item col-sm-offset-2 col-sm-8">
+              <div class='program_item__category'>
+                <?php echo $event_category; ?>
+              </div>
+              <div class="program_item__title">
+                <?php echo $event_title; ?>
+              </div>
+              <div class="program_item__date">
+                <?php echo $event_date; ?>
+              </div>
+            </div>
+
+
+          <?php }
+          wp_reset_postdata();
+    }?>
+  </div>
+</section>
