241d757efc01d1d4d19201dfc2e96f4a945c1683
enylund <nylund.eric@gmail.com>
2016-08-28 20:25:03 -0700

    inline the support blob svg

--- partials/support_button.php
+++ partials/support_button.php
@@ -1,3 +1,7 @@
-<a href="<?php the_field('support_button_link', 'option') ?>">
-  <?php the_field('support_button_text', 'option') ?>
-</a>
+
+<div class="col-sm-10 col-sm-offset-1 parent">
+  <object data="<?php echo get_bloginfo('template_url'); ?>/images/svg/support_blob.svg" type="image/svg+xml"></object>
+  <a class='child' href="<?php the_field('support_button_link', 'option') ?>">
+    <?php the_field('support_button_text', 'option') ?>
+  </a>
+</div>
