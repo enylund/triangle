72dbfe1ddd1f5589b8b540ab40b4848ad85a2f0f
enylund <nylund.eric@gmail.com>
2016-08-28 19:43:06 -0700

    Navigation styles and animation

--- header.php
+++ header.php
@@ -37,15 +37,17 @@
   <?php get_template_part('partials/utilities/ie8_notice'); ?>
   <div class="container">
     <header class="header">
-      <!--<h1 class="site-title">
-        <a href="<?php bloginfo('url'); ?>/" rel="home"><?php bloginfo('name'); ?></a>
-      </h1>-->
       <nav class="main-nav">
-        <?php
-          $args = array(
-            'theme_location' => 'primary'
-          );
-        ?>
-        <?php wp_nav_menu($args); ?>
+        <div class="main-nav__inner-wrapper">
+          <h1 class="site-title">
+            <a href="<?php bloginfo('url'); ?>/" rel="home"><?php bloginfo('name'); ?></a>
+          </h1>
+          <?php
+            $args = array(
+              'theme_location' => 'primary'
+            );
+          ?>
+          <?php wp_nav_menu($args); ?>
+        </div>
       </nav>
     </header>
