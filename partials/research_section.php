86074b667b7696e946419231d0c3b92f6adc453b
enylund <nylund.eric@gmail.com>
2016-08-28 19:43:30 -0700

    research section changes

--- partials/research_section.php
+++ partials/research_section.php
@@ -1,25 +1,27 @@
 <?php
-    //the query
-    $args = array(
-      'category_name' => 'research',
-    );
+  //the query
+  $args = array(
+    'category_name' => 'research',
+  );
 
-    $events = new WP_Query($args);
+  $events = new WP_Query($args);
 
-    if($events->have_posts()) { ?>
+  if($events->have_posts()) { ?>
+    <section class="research">
+      <div class='row'>
+        <h2 class="col-sm-offset-1 col-sm-10 site-wide__headline">
+          Research
+        </h2>
 
-      <h2 class="research__headline col-sm-12">
-        Research
-      </h2>
+        <?php while($events->have_posts()) {
+          $events->the_post();
+          $research_topic = get_field('title'); ?>
 
-      <?php while($events->have_posts()) {
-        $events->the_post();
-        $research_topic = get_field('title'); ?>
-
-        <div class="research__topic col-sm-12">
-          <?php echo $research_topic; ?>
-        </div>
-
-      <?php }
-      wp_reset_postdata();
+          <div class="research__topic col-sm-12">
+            <?php echo $research_topic; ?>
+          </div>
+      <?php } ?>
+      </div>
+    </section>
+    <?php wp_reset_postdata();
 }?>
