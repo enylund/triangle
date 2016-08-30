6cfb64f156acada0792a6e5577d99a8d1c9552b4
enylund <nylund.eric@gmail.com>
2016-08-29 19:19:22 -0700

    Support button structure and styles

--- partials/support_button.php
+++ partials/support_button.php
@@ -1,7 +1,7 @@
 
-<div class="col-sm-10 col-sm-offset-1 parent">
+<div class="col-sm-10 col-sm-offset-1 support-button-wrapper">
   <object data="<?php echo get_bloginfo('template_url'); ?>/images/svg/support_blob.svg" type="image/svg+xml"></object>
-  <a class='child' href="<?php the_field('support_button_link', 'option') ?>">
+  <a class='support-button-link' href="<?php the_field('support_button_link', 'option') ?>">
     <?php the_field('support_button_text', 'option') ?>
   </a>
 </div>
