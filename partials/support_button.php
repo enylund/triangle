545390ab20925873ddc7c1ba11746703ee3c84a4
enylund <nylund.eric@gmail.com>
2016-08-29 20:18:13 -0700

    Support button hover styles

--- partials/support_button.php
+++ partials/support_button.php
@@ -1,7 +1,9 @@
 
 <div class="col-sm-10 col-sm-offset-1 support-button-wrapper">
-  <img class='svg' src="<?php echo get_bloginfo('template_url'); ?>/images/svg/support_blob.svg" type="image/svg+xml"></object>
-  <a class='support-button-link' href="<?php the_field('support_button_link', 'option') ?>">
-    <?php the_field('support_button_text', 'option') ?>
+  <a href="<?php the_field('support_button_link', 'option') ?>">
+    <img class='svg' src="<?php echo get_bloginfo('template_url'); ?>/images/svg/support_blob.svg" type="image/svg+xml" />
+    <div class="support-button-link">
+      <?php the_field('support_button_text', 'option') ?>
+    </div>
   </a>
 </div>
