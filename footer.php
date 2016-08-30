a50a29a02e8288cc1400446a805f66b5680a79c4
enylund <nylund.eric@gmail.com>
2016-08-29 19:19:07 -0700

    Footer structure and styles

--- footer.php
+++ footer.php
@@ -1,30 +1,30 @@
-    <div class="footer__wrapper">
-    <div class="row">
-      <div class="footer__logo col-sm-6">
-        <a href="<?php bloginfo('url'); ?>/" rel="home"><?php bloginfo('name'); ?></a>
+      <div class="footer__wrapper">
+        <div class="row">
+          <div class="footer__logo col-sm-6">
+            <a href="<?php bloginfo('url'); ?>/" rel="home"><?php bloginfo('name'); ?></a>
+          </div>
+          <nav class="footer__navigation col-sm-6">
+            <div class="footer-element">
+              <?php the_field('address_line_1', 'option'); ?>
+            </div>
+            <div class="footer-element">
+              <?php the_field('address_line_2', 'option'); ?>
+            </div>
+            <div class="footer-element">
+              <?php the_field('telephone_number', 'option'); ?>
+            </div>
+            <div class="footer-element">
+              <?php the_field('email_link', 'option'); ?>
+            </div>
+            <?php
+              $args = array(
+                'theme_location' => 'footer'
+              );
+            ?>
+            <?php wp_nav_menu($args); ?>
+          </nav>
+        </div>
       </div>
-      <nav class="footer__navigation col-sm-6">
-        <div class="footer-element">
-          <?php the_field('address_line_1', 'option'); ?>
-        </div>
-        <div class="footer-element">
-          <?php the_field('address_line_2', 'option'); ?>
-        </div>
-        <div class="footer-element">
-          <?php the_field('telephone_number', 'option'); ?>
-        </div>
-        <div class="footer-element">
-          <?php the_field('email_link', 'option'); ?>
-        </div>
-        <?php
-          $args = array(
-            'theme_location' => 'footer'
-          );
-        ?>
-        <?php wp_nav_menu($args); ?>
-      </nav>
-    </div>
-    </div>
     </div><!-- .container -->
     <script>
       // (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
