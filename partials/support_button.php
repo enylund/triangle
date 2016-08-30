291b456a4e39bc3f6ecbd71f53df0b70a0d5c23d
enylund <nylund.eric@gmail.com>
2016-08-29 19:51:10 -0700

    support button svg updates

--- partials/support_button.php
+++ partials/support_button.php
@@ -1,6 +1,6 @@
 
 <div class="col-sm-10 col-sm-offset-1 support-button-wrapper">
-  <object data="<?php echo get_bloginfo('template_url'); ?>/images/svg/support_blob.svg" type="image/svg+xml"></object>
+  <img class='svg' src="<?php echo get_bloginfo('template_url'); ?>/images/svg/support_blob.svg" type="image/svg+xml"></object>
   <a class='support-button-link' href="<?php the_field('support_button_link', 'option') ?>">
     <?php the_field('support_button_text', 'option') ?>
   </a>
