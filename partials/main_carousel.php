541ec4a2f571bec53b56eef1b18b1465b66c160b
enylund <nylund.eric@gmail.com>
2016-08-29 20:54:16 -0700

    Add main homepage carousel

---
+++ partials/main_carousel.php
@@ -0,0 +1,29 @@
+<?php
+  //the query
+  $args = array(
+    'category_name' => 'main-carousel',
+  );
+
+  $main_carousel_content = new WP_Query($args);
+
+  if($main_carousel_content->have_posts()) { ?>
+    <section class="main_carousel">
+      <div class='row'>
+        <?php while($main_carousel_content->have_posts()) {
+          $main_carousel_content->the_post();
+          $main_carousel_title = get_field('title');
+          $main_carousel_season = get_field('season'); ?>
+
+          <div class="col-sm-12 main-carousel-content__title">
+            <?php echo $main_carousel_title ?>
+          </div>
+
+          <div class="col-sm-12 main-carousel-content__title">
+            <?php echo $main_carousel_season ?>
+          </div>
+
+      <?php } ?>
+      </div>
+    </section>
+    <?php wp_reset_postdata();
+}?>
