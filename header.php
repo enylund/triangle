859cfadea7ea7a8e390a1e45097a9572bcb400ed
enylund <nylund.eric@gmail.com>
2016-08-30 19:32:27 -0700

    Move nav out of header file

--- header.php
+++ header.php
@@ -36,18 +36,3 @@
 <body <?php body_class(); ?>>
   <?php get_template_part('partials/utilities/ie8_notice'); ?>
   <div class="container">
-    <header class="header">
-      <nav class="main-nav">
-        <div class="main-nav__inner-wrapper">
-          <h1 class="site-title">
-            <a href="<?php bloginfo('url'); ?>/" rel="home"><?php bloginfo('name'); ?></a>
-          </h1>
-          <?php
-            $args = array(
-              'theme_location' => 'primary'
-            );
-          ?>
-          <?php wp_nav_menu($args); ?>
-        </div>
-      </nav>
-    </header>
