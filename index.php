911251ce3bcb9aff93be459f9eeae5d1fde14a83
enylund <nylund.eric@gmail.com>
2016-08-30 19:32:41 -0700

    Include a nav partial

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php the_post() ?>
 
+<?php get_template_part('partials/nav'); ?>
 <?php get_template_part('partials/main_carousel'); ?>
 <?php get_template_part('partials/news_section'); ?>
 <?php get_template_part('partials/people_section'); ?>
